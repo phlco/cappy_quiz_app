81976b97a11c815f8af2f992b53d76959456b1f4
Morgan Neiman <quackhouse@me.com>
2013-12-02 10:20:30 -0500

    fixed sorting problem

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,21 +10,26 @@ $(document).ready(function(){
   // quizzes page
   $('#num-input').on("keyup", function(e){
 
-      // debugger;
-    if( e.which !== 32 ) {
-      // debugger;
+    if(e.which !== 32 && e.which !== 8) {
+
       var numberInput = $('#num-input').val();
-      var numberArray = numberInput.split(' ');
+      var numberArray = numberInput.split(" ");
       var originalArray = numberArray.slice(0);
-      numberArray.sort();
-      sortedString = numberArray.join("");
-      nonSortedString = originalArray.join("");
-      sortedString = sortedString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-      nonSortedString = nonSortedString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-      sortedString = sortedString.replace(/\s{2,}/g," ");
-      nonSortedString = nonSortedString.replace(/\s{2,}/g," ");
-      console.log(originalArray);
-      console.log(numberArray);
+       numericalArray = [];
+      for( i = 0; i < numberArray.length; i++ ) {
+        integer = parseInt(numberArray[i], 10);
+      // debugger;
+      numericalArray.push(integer);
+    }
+    numericalArray.sort(function(a,b) {return a-b;});
+    var  sortedString = numericalArray.join("");
+    var nonSortedString = originalArray.join("");
+    sortedString = sortedString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    nonSortedString = nonSortedString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    sortedString = sortedString.replace(/\s{2,}/g,"");
+    nonSortedString = nonSortedString.replace(/\s{2,}/g,"");
+    console.log(originalArray);
+    console.log(numericalArray);
       // debugger;
       if( sortedString === nonSortedString && !isNaN(sortedString) ) {
         $(document.body).css("background-color", "green");
@@ -32,6 +37,8 @@ $(document).ready(function(){
         $(document.body).css("background-color", "red");
       }
     }
+
+
 
   });
 
